0487261706cbaa88f7add9ab1d57f8804a46f95b
JulesMichael <julesmichaelmail@gmail.com>
2021-10-24 17:08:37 +0200

    [#1] Fix POSTGRES connection
    Fixes #1

--- app/database.py
+++ app/database.py
@@ -3,18 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-
-user = os.environ["POSTGRES_USER"]
-password = os.environ["POSTGRES_PASSWORD"]
-db = os.environ["POSTGRES_DB"]
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@0.0.0.0/{db}"
-
-print(SQLALCHEMY_DATABASE_URL)
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+POSTGRES_DB = os.environ["POSTGRES_DB"]
+POSTGRES_PORT = os.environ["POSTGRES_PORT"]
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@database:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
